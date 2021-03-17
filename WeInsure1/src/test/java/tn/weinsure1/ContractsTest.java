603851b301248e266422af89b263767caa0ea5b7
MJAOUA <PC@DESKTOP-GSQJ9GC>
2021-03-17 22:09:13 +0100

    Update Test

---
+++ WeInsure1/src/test/java/tn/weinsure1/ContractsTest.java
@@ -0,0 +1,51 @@
+package tn.weinsure1;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.weinsure1.entities.Contract;
+import tn.weinsure1.entities.ContractType;
+import tn.weinsure1.service.IContractService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContractsTest {
+
+	@Autowired 
+	IContractService ic;
+	
+	@Test		//Add Contract
+	public void TestAddContract() throws ParseException    {
+		SimpleDateFormat dateFormat = new SimpleDateFormat ("yyyy-MM-dd");
+		Date d = dateFormat.parse("2020-01-01");
+		Contract c = new Contract (d,d,5,250,ContractType.Personne,null);
+		Contract ContractAdded = ic.AddContract(c);
+		Assert.assertEquals(c.getIdcontract(), ContractAdded.getIdcontract());
+	} 
+	
+	@Test		//Update Contract
+	public void TestUpdateContract() throws ParseException    {
+		SimpleDateFormat dateFormat = new SimpleDateFormat ("yyyy-MM-dd");
+		Date d = dateFormat.parse("2018-02-02");
+		Contract c = new Contract (d,d,25,235,ContractType.Domamges,null);
+		Contract ContractUpdated = ic.UpdateContract(c);
+		Assert.assertEquals(c.getType(), ContractUpdated.getType());
+	}
+	
+	@Test		//Delete Contract from keyboard
+	public void TestDeleteContract() {
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		Long i = scanner.nextLong();
+		ic.DeleteContract(Long.toString(i)); 
+		}
+}
--- WeInsure1/src/test/java/tn/weinsure1/WeInsure1ApplicationTests.java
+++ WeInsure1/src/test/java/tn/weinsure1/WeInsure1ApplicationTests.java
@@ -1,13 +1,12 @@
 package tn.weinsure1;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WeInsure1ApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	public void contextLoads() {}
 
 }
