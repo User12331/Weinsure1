b3ab3952ceb1cd82fd28c0504cd93ff1fc493f21
MJAOUA <MJAOUA @DESKTOP-TE8KGHO>
2021-03-30 23:04:01 +0100

    Error on pushing - MJAOUA

--- WeInsure1/src/main/java/tn/weinsure1/repository/ContractRepository.java
+++ WeInsure1/src/main/java/tn/weinsure1/repository/ContractRepository.java
@@ -1,18 +1,11 @@
 package tn.weinsure1.repository;
 
-import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tn.weinsure1.entities.Contract;
 
 @Repository
 public interface ContractRepository extends CrudRepository<Contract,Long> {
-/*
-	@Query("select c from contract c where cast (c.Date_Creation as string) < %:date%")
-    List<Contract> findByYearGreater(@Param("date") String date);
 
-	*/
 }
